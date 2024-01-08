41ba1f41ee83e8b75bea74db4f8576310c1c401b
Moises Rivera <morivera2015@gmail.com>
2024-01-08 11:45:11 -0500

    compd signup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-import "./App.css";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
 }
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "mr.moises.rivera",
+            password: "FSA2310",
+          }),
+        }
+      );
+      const result = await response.json();
+      console.log("Form submitted successfully", result);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
